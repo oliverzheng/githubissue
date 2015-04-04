2e5703c6f55ce6691eec48e10f89a30ed620abc7
Oliver Zheng <gitcommits@oliverzheng.com>
2015-04-03 22:11:36 -0700

    Post on Github

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var webhook = require('express-ifttt-webhook');
 var urlParse = require('url-parse');
 var resolver = require("resolver");
+var GitHubAPI = require('github');
 
 var app = express();
 app.use(webhook(function (json, done) {
@@ -18,20 +19,52 @@ app.use(webhook(function (json, done) {
 
 function processTweet(url, text, done) {
   var pathParts = urlParse(url).pathname.split('/');
-  var author = pathParts[1];
+  var twitterUser = pathParts[1];
   var tweetID = pathParts[3];
 
-  var re = /\s+(\S+\/\S+)\s+(.*)/;
+  var re = /\s+(\S+)\/(\S+)\s+(.*)/;
   var match = re.exec(text);
   if (match !== null) {
-    var repo = match[1];
-    var problem = match[2];
-    console.log('@' + author + ' filed an issue in ' + repo + ': ' + problem);
+    var githubUser = match[1]
+    var repo = match[2];
+    var problem = match[3];
+    console.log('@' + twitterUser + ' filed an issue in ' + repo + ': ' + problem);
+    postOnGithub(githubUser, repo, twitterUser, problem, url, done);
   } else {
     console.log('Invalid tweet: ' + text);
+    done();
   }
+}
 
-  done();
+var github = new GitHubAPI({
+  version: '3.0.0',
+});
+github.authenticate({
+  type: 'basic',
+  username: 'githubissue',
+  password: process.env.GITHUB_PASSWORD,
+});
+
+function postOnGithub(githubUser, repo, twitterUser, problem, twitterUrl, done) {
+  github.issues.create({
+    user: githubUser,
+    repo: repo,
+    title: problem,
+    body: '@' + twitterUser + ' filed this issue from ' + twitterUrl,
+    labels: [],
+  }, function(err, res) {
+    if (err) {
+      console.log('Github error trying to do ' + twitterUrl);
+    } else {
+      var issueUrl = res.html_url;
+      console.log('Posted issue: ' + issueUrl);
+      postOnTwitter();
+    }
+    done();
+  });
+}
+
+function postOnTwitter() {
 }
 
 app.get('/', function (req, res) {
