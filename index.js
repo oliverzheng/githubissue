058a84198beed720e2431f229c2765d494d537fc
Oliver Zheng <gitcommits@oliverzheng.com>
2015-04-03 21:33:26 -0700

    Add tweet parsing logic.

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var webhook = require('express-ifttt-webhook');
+var urlParse = require('url-parse');
 
 var app = express();
 app.use(webhook(function (json, done) {
-  console.log('Got', json);
+  var url = urlParse(json.title);
+  var pathParts = url.pathname.split('/');
+  var author = pathParts[1];
+  var tweetID = pathParts[3];
+
+  var text = json.description;
+  var re = /\s+(\S+\/\S+)\s+(.*)/;
+  var match = re.exec(text);
+  if (match !== null) {
+    var repo = match[1];
+    var problem = match[2];
+    console.log('@' + author + ' filed an issue in ' + repo + ': ' + problem);
+  } else {
+    console.log('Invalid tweet: ' + text);
+  }
+
   done();
 }));
 
